cb006da0c6db4ec97e87c938e11593c0af6255fd
hrlds <haroldespinoza823@gmail.com>
2023-08-18 00:48:05 -0600

    xd

---
+++ Tarea/software/cronometrof/obj/default/hello_world_small.d
@@ -0,0 +1,29 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../cronometrof_bsp//HAL/inc/sys/alt_stdio.h \
+ ../cronometrof_bsp//HAL/inc/sys/alt_irq.h \
+ ../cronometrof_bsp//HAL/inc/nios2.h \
+ ../cronometrof_bsp//HAL/inc/alt_types.h ../cronometrof_bsp/system.h \
+ ../cronometrof_bsp/linker.h \
+ ../cronometrof_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../cronometrof_bsp//HAL/inc/io.h ../cronometrof_bsp//HAL/inc/alt_types.h \
+ ../cronometrof_bsp//drivers/inc/altera_avalon_timer_regs.h
+
+../cronometrof_bsp//HAL/inc/sys/alt_stdio.h:
+
+../cronometrof_bsp//HAL/inc/sys/alt_irq.h:
+
+../cronometrof_bsp//HAL/inc/nios2.h:
+
+../cronometrof_bsp//HAL/inc/alt_types.h:
+
+../cronometrof_bsp/system.h:
+
+../cronometrof_bsp/linker.h:
+
+../cronometrof_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../cronometrof_bsp//HAL/inc/io.h:
+
+../cronometrof_bsp//HAL/inc/alt_types.h:
+
+../cronometrof_bsp//drivers/inc/altera_avalon_timer_regs.h:
